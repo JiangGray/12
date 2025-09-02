7372c3de1dd9ee741da47200749f9a9e866e87f8
1600226 <1600226@systex.com.tw>
2025-09-02 15:15:57 +0800

    momo

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ let tables = {}; // { tableId: { players: [], boardState: ..., turn: 1 } }
 io.on("connection", socket => {
   console.log("玩家連線:", socket.id);
 
+  // 傳送大廳資料
   socket.emit("lobbyUpdate", Object.keys(tables));
 
   // 建立桌子
@@ -40,28 +41,5 @@ io.on("connection", socket => {
     }
   });
 
-  // 玩家同步棋盤
-  socket.on("move", ({ tableId, newState, nextTurn }) => {
-    const table = tables[tableId];
-    if (table) {
-      table.boardState = newState;
-      table.turn = nextTurn;
-      table.players.forEach(p => {
-        io.to(p).emit("updateState", { boardState: newState, turn: nextTurn });
-      });
-    }
-  });
-
-  // 玩家離線
-  socket.on("disconnect", () => {
-    console.log("玩家離線:", socket.id);
-    for (const [id, table] of Object.entries(tables)) {
-      table.players = table.players.filter(p => p !== socket.id);
-      if (table.players.length === 0) delete tables[id];
-    }
-    io.emit("lobbyUpdate", Object.keys(tables));
-  });
-});
-
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log("伺服器運行在 http://localhost:" + PORT));
+  // 同步棋盤
+  socket.on("move", ({ tableId, newS
